ee2ebbdc76431b4a93fe0fd20b1e05e526853d9d
Harsha Kumarasingha <harshakumara1998030944@gmail.com>
2024-12-08 19:51:01 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 
 console.log("DB_USER Name", process.env.DB_USER);
@@ -12,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection code
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://harshakumara1998030944:o8P8L7mFO9MMocIZ@learnora.h7gt7.mongodb.net/?retryWrites=true&w=majority&appName=learnora`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,7 +39,6 @@ async function connectToDatabase() {
     coursecollection = database.collection("course");
     studentcollection = database.collection("students");
     teachercollection = database.collection("teachers");
-    resultcollection = database.collection("results");
     subjectcollection = database.collection("subjects");
     admincollection = database.collection("admins");
     cartcollection = database.collection("carts");
@@ -82,13 +82,56 @@ app.get('/course',async(req,res)=>{
   res.send(result);
 })
 //get class by 
-app.get('/course/:owner_id', async(req,res)=>{
-  const owner_id=req.params.owner_id;
-  const query={owner_id:owner_id};
+app.get('/course/id', async(req,res)=>{
+  const id=req.params.id;
+  const query={owner_id:id};
   const result=await coursecollaction.find(query).toArray();
   res.send(result);
 })
 
+app.get('/new-course', async(req,res)=>{
+  const result = await coursecollection.find().toArray();
+  res.send(result);
+})
+//update classes title and reason
+
+app.put('/update-class/:id',async(req,res)=>{
+  const id=req.params.id;
+  const title=res.body.title;
+  const reason=res.body.reason; 
+  const filter={_id:new ObjectId(id)};
+  const updateDoc={
+    $set:{
+      title:title,
+      reason:reason
+    },
+  };
+  const result=await coursecollection.updateOne(filter,updateDoc);
+  res.send(result);  
+})
+//update all class
+app.put('/update-class/:id',async(req, res)=>{
+      const id=req.params.id;
+      const updateclass=req.body;
+      const filter={_id:new ObjectId(id)};
+      const options = { upsert: true };
+      const updateDoc={
+        $set:{
+          title:updateclass.title,
+          descriptio:updateclass.description
+
+        }
+      };
+      const result=await coursecollection.updateOne(filter,updateDoc,options);
+      res.send(result);
+})
+
+//card collaction
+app.post('/add-to-cart',async(req,res)=>{
+    const newcart=req.body;
+    const result=await cartcollection.insertOne(newcart);
+    res.send(result);
+})
 app.get('/', (req, res) => {
   res.send('Hello Developer, how are you? I am Harsha Kumarasingha!');
 });
